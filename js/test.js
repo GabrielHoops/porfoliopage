95806448022acf17c3d2360fd1784546a3a121ea
Hoops <gabriel.hoops@adesso.de>
2022-04-11 11:52:20 +0200

    bugfixing

--- js/test.js
+++ js/test.js
@@ -114,7 +114,7 @@ function drawScore() {
 function drawLives() {
     ctx.font = "16px Arial";
     ctx.fillStyle = "#0095DD";
-    ctx.fillText("Lives: "+lives, canvas.width-65, 20);
+    ctx.fillText("Leben: "+lives, canvas.width-65, 20);
 }
 
 function draw() {
